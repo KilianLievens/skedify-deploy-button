9bfc1663cf12d940b0670ffcd01716dd4ea5d6fc
Kilian Lievens <Kilian.Lievens@telenet.be>
2020-07-30 00:05:28 +0200

    stylize deploy output

--- deploy_button.py
+++ deploy_button.py
@@ -1,10 +1,20 @@
-import RPi.GPIO as GPIO
-import os
-import time
 from datetime import datetime
+from os import system as shell
 from threading import Thread
+from time import sleep
+
+from asciimatics.effects import Julia, Print, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from pyfiglet import figlet_format
+
+import RPi.GPIO as GPIO
 
 #### CONSTANTS ####
+
+SAFE_MODE = True
+DRY_RUN_MODE = True
 
 # BUTTONS
 DEPLOY_BUTTON_PIN = 29
@@ -43,16 +53,41 @@ GIT_LATEST = '''\
 
 DEPLOY_TEMPLATE = '''\
     kubectl config set-cluster {cluster_name} \
-    && ytt -f {k8s_folder}/{environment}/cluster-configuration | kubectl apply --validate=false -f - \
-    && {k8s_folder}/get-all-enterprise-yaml.sh {environment} | kubectl apply -f - \
+    && ytt -f {k8s_folder}/{environment}/cluster-configuration | kubectl apply --validate=false -f - | grep --color=always -e "^" -e "configured" \
+    && {k8s_folder}/get-all-enterprise-yaml.sh {environment} | kubectl apply -f - | grep --color=always -e "^" -e "configured" \
     '''
 DRY_RUN_DEPLOY_TEMPLATE = '''\
     kubectl config set-cluster {cluster_name} \
-    && ytt -f {k8s_folder}/{environment}/cluster-configuration | cat - \
-    && {k8s_folder}/get-all-enterprise-yaml.sh {environment} | cat - \
+    && ytt -f {k8s_folder}/{environment}/cluster-configuration | kubectl diff -f - \
+    && {k8s_folder}/get-all-enterprise-yaml.sh {environment} | kubectl diff -f - \
     '''
 
+#### EFFECTS ####
+
+
+def big_text_effect(screen, text):
+    effects = [
+        Print(
+            screen,
+            FigletText(text, font='big'),
+            screen.height // 2 - 8),
+        Stars(screen, (screen.width + screen.height) // 2)
+    ]
+    screen.play([Scene(effects, 50)], repeat=False)
+
+
+def yolo_mode_effect(screen):
+    effects = [
+        Print(
+            screen,
+            FigletText('YOLOOOO', font='big'),
+            screen.height // 2 - 8),
+        Julia(screen)
+    ]
+    screen.play([Scene(effects, 50)], repeat=False)
+
 #### CALLBACKS ####
+
 
 def beeper_daemon():
     rotator = False
@@ -64,40 +99,65 @@ def beeper_daemon():
 
         rotator = not rotator
         GPIO.output(BEEPER_OUT_PIN, rotator)
-        time.sleep(0.5)
+        sleep(0.5)
 
 
 def switch_callback(channel):
     # Give the switch some time to settle (physics yo)
-    time.sleep(0.05)
+    sleep(0.05)
 
-    if GPIO.input(channel):
-        print(SWITCHES.get(channel, "Invalid switch number").upper())
+    if not GPIO.input(channel):
+        return
 
-        if channel == PRODUCTION_SWITCH_PIN:
-            # Production ENV gets warning beeps
-            thread = Thread(target=beeper_daemon, args=())
-            thread.daemon = True
-            thread.start()
+    if channel == PRODUCTION_SWITCH_PIN:
+        # Production ENV gets warning beeps
+        thread = Thread(target=beeper_daemon, args=())
+        thread.daemon = True
+        thread.start()
+
+    if all(GPIO.input(ch) for ch in SWITCHES):
+        Screen.wrapper(yolo_mode_effect)
+        return
+
+    enabled_env = SWITCHES.get(channel, "Invalid switch number").upper()
+    Screen.wrapper(big_text_effect, arguments=[enabled_env])
 
 
 def deploy_button(channel):
-    print('#### DEPLOYING ####')
-    commands = [GIT_LATEST]
-    for pin, env in SWITCHES.items():
-        # TODO rickroll if no switches
-        if GPIO.input(pin):
-            commands.append(
-                DRY_RUN_DEPLOY_TEMPLATE.format(
-                    cluster_name=CLUSTER_NAMES[env],
-                    environment=env,
-                    k8s_folder=VIDEO_K8S_FOLDER
-                )
+    template = DRY_RUN_DEPLOY_TEMPLATE if DRY_RUN_MODE else DEPLOY_TEMPLATE
+
+    if SAFE_MODE:
+        sleep(0.5)
+        if(not GPIO.input(channel)):
+            print('Safe mode is on. Press the deploy button for at least half a second. Button cooldown is 3 seconds')
+            return
+
+    print(figlet_format('DEPLOYING'))
+
+    commands = [GIT_LATEST] # Make sure video-k8s is up to date
+    environments_to_deploy = [
+        [pin, env] for pin, env in SWITCHES.items() if GPIO.input(pin)
+    ]
+
+    if not environments_to_deploy:
+        shell('bash ./roll.sh')  # Hit em with the man
+        return
+
+    for pin, env in environments_to_deploy:
+        commands.append(
+            template.format(
+                cluster_name=CLUSTER_NAMES[env],
+                environment=env,
+                k8s_folder=VIDEO_K8S_FOLDER
             )
-    os.system('&& '.join(commands))
-    # TODO add output highlighting
+        )
+    shell('&& '.join(commands))
+
+    print(figlet_format('DONE'))
 
 #### MAIN ####
+
+
 try:
     # GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)  # Physical numbering
@@ -116,7 +176,7 @@ try:
         DEPLOY_BUTTON_PIN,
         GPIO.RISING,
         callback=deploy_button,
-        bouncetime=5000  # Avoid someone double pressing within 5 seconds
+        bouncetime=3000  # Avoid someone double pressing within 3 seconds
     )
 
     GPIO.setup(BEEPER_OUT_PIN, GPIO.OUT)
@@ -124,6 +184,5 @@ try:
     message = input("LETS GO!\n")
 
 finally:
-    # Always cleanup
-    GPIO.cleanup()
-    print('cleaned up')
+    GPIO.cleanup() # Always cleanup
+    print(figlet_format('Cleaned up. Goodbye.'))
